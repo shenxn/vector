b7c38c4e34871189363c9c4f6d2bef049da6628c
prognant <pierre.rognant@datadoghq.com>
2021-09-30 14:56:38 +0200

    feat(datadog_agent source): traces support early work
    Signed-off-by: prognant <pierre.rognant@datadoghq.com>

--- src/sources/datadog/mod.rs
+++ src/sources/datadog/mod.rs
@@ -1,2 +1,14 @@
-#[cfg(feature = "sources-datadog_agent")]
+#[cfg(test)]
+mod tests;
+
 pub mod agent;
+pub mod config;
+pub mod logs;
+pub mod traces;
+
+use crate::config::SourceDescription;
+use crate::sources::datadog::config::DatadogAgentConfig;
+
+inventory::submit! {
+    SourceDescription::new::<DatadogAgentConfig>("datadog_agent")
+}
