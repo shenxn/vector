d8f2973f3fdd1fcec62c100da1235a80bf0e8a93
prognant <pierre.rognant@datadoghq.com>
2021-11-26 18:19:37 +0100

    +refactor
    Signed-off-by: prognant <pierre.rognant@datadoghq.com>

--- src/sources/datadog/mod.rs
+++ src/sources/datadog/mod.rs
@@ -2,12 +2,11 @@
 mod tests;
 
 pub mod agent;
-pub mod config;
 pub mod logs;
 pub mod traces;
 
 use crate::config::SourceDescription;
-use crate::sources::datadog::config::DatadogAgentConfig;
+use crate::sources::datadog::agent::DatadogAgentConfig;
 
 inventory::submit! {
     SourceDescription::new::<DatadogAgentConfig>("datadog_agent")
